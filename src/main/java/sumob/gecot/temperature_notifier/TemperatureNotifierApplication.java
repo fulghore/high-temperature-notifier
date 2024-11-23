13a1cdb65b4d0a82dfc614736e3913ff5e33d936
Aldrin Lima <aldrin.bl17@gmail.com>
2024-11-23 16:11:35 -0300

    Terceira tentativa de publicação no Railway.

--- src/main/java/sumob/gecot/temperature_notifier/TemperatureNotifierApplication.java
+++ src/main/java/sumob/gecot/temperature_notifier/TemperatureNotifierApplication.java
@@ -4,10 +4,11 @@ import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @OpenAPIDefinition(servers = {@Server(url = "/", description = "Default Server URL")})
 @SpringBootApplication
-public class TemperatureNotifierApplication {
+public class TemperatureNotifierApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TemperatureNotifierApplication.class, args);
